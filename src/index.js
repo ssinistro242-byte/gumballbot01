8999fcc858a73a700d2675d970cb1e9e800e8df9
ssinistro242-byte <ssinistro242@gmail.com>
2026-04-02 14:42:12 -0300

    Add HTTP server to respond with 'Bot online'

--- src/index.js
+++ src/index.js
@@ -144,3 +144,12 @@ async function startBot() {
 }
 
 startBot();
+
+const PORT = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+  res.write("Bot online");
+  res.end();
+}).listen(PORT, () => {
+  console.log("Servidor ativo na porta " + PORT);
+});
